f64c909e4162cc638304d4208c27be279923f26a
Roshni <20968692+roshni-github@users.noreply.github.com>
2026-05-20 22:06:13 +0530

    Update streamlit_app.py - changed script to logging

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
-import streamlit as st
+# import streamlit as st
 
-print("Hello Roshni")
-st.title("💃🏼 Hello Streams 🎊")
+# print("Hello Roshni")
+# st.title("💃🏼 Hello Streams 🎊")
+
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logging.info("Hello Roshni")
+
